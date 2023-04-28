ce6d53c8d053245e38fbcd2e019c48efb2835a2f
Jackie Li <li.jackie565@gmail.com>
2023-04-28 01:26:23 -0400

    code mirror packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-function App() {
-  return <></>;
-}
+import React from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { sublime } from "@uiw/codemirror-theme-sublime";
 
+function App() {
+  return (
+    <>
+      <div className="w-screen bg-slate-600 h-screen flex justify-center items-center">
+        <CodeMirror
+          value="console.log('hello world!');"
+          height="200px"
+          theme={sublime}
+          width="400px"
+          extensions={[javascript({ jsx: true })]}
+        />
+      </div>
+    </>
+  );
+}
 export default App;
