5e8dd07b6935f177a4e0c4e1432c1c6ec83efdac
Jackie Li <li.jackie565@gmail.com>
2023-05-01 00:27:15 -0400

    Small change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import { sublime } from "@uiw/codemirror-theme-sublime";
@@ -7,6 +8,18 @@ const bp = "function Sum(a, b) {}";
 
 function App() {
   const [code, setCode] = useState(bp);
+
+  const submit = async () => {
+    try {
+      const req = await axios.post("http://localhost:80/javascript", {
+        code: code,
+      });
+      const res = req.data;
+      console.log(res);
+    } catch {
+      console.log("Server Error");
+    }
+  };
 
   return (
     <>
@@ -25,6 +38,12 @@ function App() {
             console.log(code);
           }}
         />
+        <button
+          className="m-3 bg-slate-300 h-10 w-32 rounded-md text-xl"
+          onClick={submit}
+        >
+          Submit
+        </button>
       </div>
     </>
   );
