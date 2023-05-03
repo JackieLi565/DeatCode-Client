2d5f932e7175aa1b6396c673047bc069c5fc0c81
Jackie Li <li.jackie565@gmail.com>
2023-05-03 19:21:36 -0400

    Added Proxy

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
-import CodeMirror from "@uiw/react-codemirror";
-import { javascript } from "@codemirror/lang-javascript";
-import { sublime } from "@uiw/codemirror-theme-sublime";
+import CodeBox from "./components/CodeBox";
 
 const bp = "function Sum(a, b) {}";
 
 function App() {
-  const [code, setCode] = useState(bp);
-
-  const submit = async () => {
+  const submit = async (code) => {
+    console.log(code);
     try {
       const req = await axios.post("http://localhost:8080/javascript", {
         code: code,
@@ -23,28 +20,7 @@ function App() {
 
   return (
     <>
-      <div className="w-screen bg-slate-600 h-screen flex flex-col justify-center items-center">
-        <h1 className="m-4 text-gray-200 text-xl">
-          Create a function tha returns the sum of two numbers{" "}
-        </h1>
-        <CodeMirror
-          value={code}
-          height="200px"
-          theme={sublime}
-          width="500px"
-          extensions={[javascript({ jsx: true })]}
-          onChange={(value) => {
-            setCode(value);
-            console.log(code);
-          }}
-        />
-        <button
-          className="m-3 bg-slate-300 h-10 w-32 rounded-md text-xl"
-          onClick={submit}
-        >
-          Submit
-        </button>
-      </div>
+      <CodeBox submit={submit} />
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
-    "/api/": {
-      target: "http://localhost:55714/",
-      changeOrigin: true,
-      secure: false,
+    proxy: {
+      "/api/": "http://localhost:55714/",
     },
   },
+  plugins: [react()],
 });
